745919237b769cda5bde8e433d6cdbfe93919a5d
Raslav Milutinovic <admin@rasovica.com>
2019-08-10 12:49:12 +0200

    feat: Add typescript

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import * as React from "react";
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <html>
+            <Head>
+                <style>{`body { margin: 0 } /* custom! */`}</style>
+            </Head>
+            <body className="custom_class">
+            <Main />
+            <NextScript />
+            </body>
+            </html>
+        )
+    };
+}
